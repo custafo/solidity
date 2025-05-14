3e502db908abffe624f0beb97554a4182d4220f9
Gustavo Pereira Oliveira Lima <101191931+custafo@users.noreply.github.com>
2025-05-14 16:24:57 +0000

    get avrg done

---
+++ projects/gradeAverage.js
@@ -0,0 +1,15 @@
+function getAverage(scores) {
+if(scores.length === 0){
+return 0;
+}
+
+let total = 0;
+for(let i = 0; i < scores.length; i++){
+  total += scores[i];
+}
+const average = total / scores.length;
+return average;
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
